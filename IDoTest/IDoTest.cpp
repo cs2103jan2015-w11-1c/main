28d3d9ef5157fe629f2537111bf05beb70becab5
shenyeelah <yee940908@hotmail.com>
2015-03-04 11:37:42 +0800

    Test
    Test

---
+++ IDoTest/IDoTest.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace IDoTest
+{		
+	TEST_CLASS(IDoTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}

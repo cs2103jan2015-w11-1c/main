3e0398b6a15b8e15e1a8acbefc7eba31c6611bd9
leexinni <xinnnnnnni@gmail.com>
2015-03-24 12:22:27 +0800

    added Mark class

---
+++ IDo/Mark.cpp
@@ -0,0 +1,79 @@
+#include "Mark.h"
+Mark::Mark() {
+}
+
+Mark::~Mark() {
+}
+
+//checks if input number falls within size of task list
+bool Mark::isValidInput(int userinput, int taskListSize) {
+	//cout << "taskListSize = " << taskListSize << endl;
+
+	if (userinput <= taskListSize) {
+		_indextomark = userinput - 1;
+		
+		return true;
+	} else {
+		return false;
+	}
+	
+}
+
+vector<Task> Mark::execute(vector<string> parsedInfo, vector<Task> taskListFromLogic) {
+	_typetomark = parsedInfo[2];
+	
+	_contenttomark = parsedInfo[3];
+	
+	Task tasktomark = taskListFromLogic[_indextomark];
+	Task marked;
+	
+	if (_typetomark == "status") {
+		marked = markStatus(tasktomark);
+	} else if (_typetomark == "priority") {
+		marked = markPriority(tasktomark);
+	}
+	
+	taskListFromLogic[_indextomark] = marked;
+	return taskListFromLogic;
+}
+
+
+Task Mark::markStatus (Task tasktomark) {
+	if (_contenttomark == "yes") {
+		tasktomark.setStatus(COMPLETED);
+	} else if (_contenttomark == "no") {
+		tasktomark.setStatus(UNCOMPLETED);
+	} 
+	
+	return tasktomark;
+}
+
+Task Mark::markPriority (Task tasktomark) {
+	if (_contenttomark == "high") {
+		tasktomark.setPriority(HIGH);
+	} else if (_contenttomark == "medium") {
+		tasktomark.setPriority(MEDIUM);
+	} else if (_contenttomark == "low") {
+		tasktomark.setPriority(LOW);
+	} else if (_contenttomark == "none") {
+		tasktomark.setPriority(NONE);
+	}
+
+	return tasktomark;
+}
+
+Task Mark::markLabel (Task tasktomark) {
+	if (_contenttomark == "studies") {
+		tasktomark.setLabel(STUDIES);
+	} else if (_contenttomark == "CCA") {
+		tasktomark.setLabel(CCA);
+	} else if (_contenttomark == "family") {
+		tasktomark.setLabel(FAMILY);
+	} else if (_contenttomark == "friends") {
+		tasktomark.setLabel(FRIENDS);
+	} else if (_contenttomark == "misc") {
+		tasktomark.setLabel(MISC);
+	}
+
+	return tasktomark;
+}
---
+++ IDo/Mark.h
@@ -0,0 +1,28 @@
+#ifndef MARK_H
+#define MARK_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Logic.h"
+#include "Task.h"
+
+using namespace std;
+
+class Mark {
+private:
+	int _indextomark;
+	string _typetomark;
+	string _contenttomark;
+
+public:
+	Mark();
+	~Mark();
+	bool isValidInput(int, int);
+	vector<Task> execute (vector<string> parsedInformation, vector<Task> taskListFromLogic);
+	Task markStatus (Task);
+	Task markPriority (Task);
+	Task markLabel (Task);
+};
+
+#endif
